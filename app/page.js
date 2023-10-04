d92d27e635c2e7936953fe3832b26d5c44148186
Nwankwo Henry <nameisifunanya@gmail.com>
2023-10-04 11:15:05 +0100

    Added some stylings

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import ProgressBar from '@/components/ProgressBar';
+import MyDropzone from '@/components/DropZone';
+import FilesGroup from '@/components/FilesGroup';
 
 export default function Home() {
   return (
-    <section className='w-full flex justify-center items-center'>
-      <ProgressBar progressText={'processing...'} progressValue={4} />
+    <section className='w-full flex flex-col justify-center items-center py-8 md:px-8'>
+      <MyDropzone dropzoneStyles={'xtr-dropzone'}></MyDropzone>
+      <FilesGroup></FilesGroup>
     </section>
   );
 }
