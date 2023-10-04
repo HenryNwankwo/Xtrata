d7b51048bed3d5e21443ce0cc32ab4b5cc0d5a21
Nwankwo Henry <nameisifunanya@gmail.com>
2023-10-04 11:06:09 +0100

    Added a state holder for the toggling of the file group

--- utils/XtrataContext.js
+++ utils/XtrataContext.js
@@ -5,8 +5,10 @@ const XtrataContext = createContext();
 
 export const XtrataProvider = ({ children }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
   //The object values of the provider
-  const providerValues = { isMenuOpen, setIsMenuOpen };
+  const providerValues = { isMenuOpen, setIsMenuOpen, isOpen, setIsOpen };
 
   return (
     <XtrataContext.Provider value={providerValues}>
