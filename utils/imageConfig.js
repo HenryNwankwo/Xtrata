e137b06fde1c871fab3e9f5018e777d297ba9895
Nwankwo Henry <nameisifunanya@gmail.com>
2023-10-09 12:29:07 +0100

    Updated the image config with the new images of different documents

--- utils/imageConfig.js
+++ utils/imageConfig.js
@@ -3,6 +3,11 @@ import logoBlue from '@/public/assets/img/logo-blue.png';
 import logoGray from '@/public/assets/img/logo-gray.png';
 import checkIcon from '@/public/assets/img/check.png';
 import extractIcon from '@/public/assets/img/extract.png';
+import txtFile from '@/public/assets/img/txtfile.png';
+import excelFile from '@/public/assets/img/excel.png';
+import mswordFile from '@/public/assets/img/msword.png';
+import pdfFile from '@/public/assets/img/pdf.png';
+import defaultFile from '@/public/assets/img/file.png';
 
 export const imageConfig = {
   blueLogo: logoBlue,
@@ -10,4 +15,13 @@ export const imageConfig = {
   grayLogo: logoGray,
   checkIcon: checkIcon,
   extractIcon: extractIcon,
+  txt: txtFile,
+  plain: txtFile,
+  doc: mswordFile,
+  docx: mswordFile,
+  msword: mswordFile,
+  xls: excelFile,
+  xlsx: excelFile,
+  pdf: pdfFile,
+  default: defaultFile,
 };
