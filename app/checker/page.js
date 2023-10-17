f18aeb746c937f2da43fe8ba449cef23a778b8c8
Nwankwo Henry <nameisifunanya@gmail.com>
2023-10-17 14:17:37 +0100

    Finished styling te grouped dropzones

--- app/checker/page.js
+++ app/checker/page.js
@@ -1,7 +1,11 @@
 import GroupedDropzone from '@/components/GroupedDropzone';
 
 function page() {
-  return <GroupedDropzone></GroupedDropzone>;
+  return (
+    <section className='w-full h-auto flex flex-col justify-center items-center py-8 md:px-8'>
+      <GroupedDropzone></GroupedDropzone>;
+    </section>
+  );
 }
 
 export default page;
--- components/GroupedDropzone.jsx
+++ components/GroupedDropzone.jsx
@@ -10,17 +10,27 @@ function GroupedDropzone() {
     setPartBRejectedFiles,
   } = useXtrataContext();
   return (
-    <section>
-      <MyDropzone
-        dropzoneStyles={'xtr-dropzone'}
-        setFiles={setPartAFiles}
-        setTheRejectedFiles={setPartARejectedFiles}
-      />
-      <MyDropzone
-        dropzoneStyles={'xtr-dropzone'}
-        setFiles={setPartBFiles}
-        setTheRejectedFiles={setPartBRejectedFiles}
-      />
+    <section className='w-full h-auto flex flex-col md:flex-row items-center justify-between'>
+      <article className='md:w-6/12'>
+        <h3 className='w-full md:w-1/2 md:ml-auto -mb-0.5 md:rounded-tl-full py-2 px-4 text-center text-white bg-orange-600'>
+          Files to be checked
+        </h3>
+        <MyDropzone
+          dropzoneStyles={'xtr-dropzone '}
+          setFiles={setPartAFiles}
+          setTheRejectedFiles={setPartARejectedFiles}
+        />
+      </article>
+      <article className='md:w-6/12 mt-4 md:mt-0'>
+        <h3 className='w-full md:w-1/2 -mb-0.5 md:rounded-tr-full py-2 px-4 text-center text-white bg-blue-950 '>
+          Files for searching
+        </h3>
+        <MyDropzone
+          dropzoneStyles={'xtr-dropzone'}
+          setFiles={setPartBFiles}
+          setTheRejectedFiles={setPartBRejectedFiles}
+        />
+      </article>
     </section>
   );
 }
